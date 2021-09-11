421411c604150f56b55cbc98692605f4ec94e175
JUYEON_LEE <cavss1118@gmail.com>
2021-09-11 19:45:25 +0900

    #0017 , profile user DTO dummy data

--- app/src/main/java/com/example/fooddelivery/util/dummy/Dummy.kt
+++ app/src/main/java/com/example/fooddelivery/util/dummy/Dummy.kt
@@ -2,12 +2,24 @@ package com.example.fooddelivery.util.dummy
 
 import com.example.fooddelivery.R
 import com.example.fooddelivery.models.MainProductModel
+import com.example.fooddelivery.models.UserModel
 
 object Dummy {
 
-    val mainProducts : List<MainProductModel> = listOf<MainProductModel>(
-        MainProductModel("당근", R.drawable.image_email),
-        MainProductModel("오이", R.drawable.image_email),
-        MainProductModel("딸기", R.drawable.image_email)
+
+    val user = UserModel(
+        userName = "김수한무",
+        userAddress = "서울시 반포구 반포동포동",
+        userEmail = "cavss1118@gmail.com",
+        userSelfie = "아직 이미지 데이터 없음",
+        userSafe = false,
+        userUID = "uid 1111",
+        userPosts = listOf("postUID1", "postUID2", "postUID3", "postUID4","postUID5"),
+        userCAM = listOf("Carrot cam1", "Carrot cam2", "Carrot cam3", "Carrot cam4"),
+        userMainProduct = listOf<MainProductModel>(
+            MainProductModel("당근", R.drawable.image_email),
+            MainProductModel("오이", R.drawable.image_email),
+            MainProductModel("딸기", R.drawable.image_email)
+        )
     )
 }

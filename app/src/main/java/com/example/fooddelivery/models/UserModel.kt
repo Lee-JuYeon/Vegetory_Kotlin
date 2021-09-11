e6db1e2462a155c3f7c400f5c0825084f11925d3
JUYEON_LEE <cavss1118@gmail.com>
2021-09-11 19:40:56 +0900

    #0016 , profile user DTO

---
+++ app/src/main/java/com/example/fooddelivery/models/UserModel.kt
@@ -0,0 +1,15 @@
+package com.example.fooddelivery.models
+
+data class UserModel (
+
+    val userName : String, // 유저이름
+    val userAddress : String, // 유저 거주지
+    val userEmail : String, // 유저 이메일
+    val userSelfie : String, // 유저 셀카
+    val userSafe : Boolean, // 유저 인증
+    val userUID : String, // 유저 uid
+    val userPosts : List<String>, // 유저 포스트 uid
+    val userCAM : List<String>, // 유저 캠 json path
+    val userMainProduct : List<MainProductModel> // 유저 주 생상품목
+
+    )

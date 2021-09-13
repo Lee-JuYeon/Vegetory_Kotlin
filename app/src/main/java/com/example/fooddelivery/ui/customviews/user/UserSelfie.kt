585d8ca721b7521184cb2c1102ad012fde4d787a
JUYEON_LEE <cavss1118@gmail.com>
2021-09-13 20:18:22 +0900

    #0031 , Refactory UserSelfie parameter

--- app/src/main/java/com/example/fooddelivery/ui/customviews/user/UserSelfie.kt
+++ app/src/main/java/com/example/fooddelivery/ui/customviews/user/UserSelfie.kt
@@ -3,28 +3,45 @@ package com.example.fooddelivery.ui.views.profile
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.fooddelivery.R
 
 @Composable
-fun ProfileSelfie(
-    setModifier : Modifier
+fun UserSelfie(
+    setLength : Dp,
+    setImage : Int
 ) {
+    val radius = setLength / 3
     Image(
-        painter = painterResource(R.drawable.image_email),
+        painter = painterResource(setImage),
         contentDescription = null,
-        modifier = setModifier
-//        modifier = Modifier
-//            .size(setSize)
-//            .clip(CircleShape)
-//            .border(1.5.dp, MaterialTheme.colors.primaryVariant, CircleShape)
+        modifier = Modifier
+            .padding(3.dp)
+            .size(setLength, setLength)
+            .clip(
+                RoundedCornerShape(
+                    corner = CornerSize(radius)
+                )
+            )
+            .background(Color.Red)
+            .border(
+                1.dp,
+                MaterialTheme.colors.primaryVariant,
+                RoundedCornerShape(
+                    corner = CornerSize(radius)
+                )
+            )
     )
 }

002fb4c8ccce4c02643a89555948e94c7adf0215
JUYEON_LEE <cavss1118@gmail.com>
2021-09-16 20:54:25 +0900

    #0045 , BottomSheet example url

---
+++ app/src/main/java/com/example/fooddelivery/ui/customviews/bottomsheet/BottomSheet.kt
@@ -0,0 +1,49 @@
+package com.example.fooddelivery.ui.customviews.bottomsheet
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+
+class BottomSheet {
+}
+
+@Composable
+fun TransformableSample() {
+    // set up all transformation states
+    var scale by remember { mutableStateOf(1f) }
+    var rotation by remember { mutableStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+        scale *= zoomChange
+        rotation += rotationChange
+        offset += offsetChange
+    }
+    Box(
+        Modifier
+            // apply other transformations like rotation and zoom
+            // on the pizza slice emoji
+            .graphicsLayer(
+                scaleX = scale,
+                scaleY = scale,
+                rotationZ = rotation,
+                translationX = offset.x,
+                translationY = offset.y
+            )
+            // add transformable to listen to multitouch transformation events
+            // after offset
+            .transformable(state = state)
+            .background(Color.Blue)
+            .fillMaxHeight()
+    )
+}
+
+/*
+https://ichi.pro/ko/jetpack-composeleul-sayonghayeo-bottomsheet-kontencheue-aenimeisyeon-eul-jeog-yonghaneun-bangbeob-68906696522716
+ */

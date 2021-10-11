2caf8d092f311f1e975ac23ddef34861d06f2991
JuYeon Lee <cavss1118@gmail.com>
2021-10-11 15:15:17 +0900

    #0070, CustomSheet// 1. Refactoring 2. State & Bind acheive for appear and disappear animation 3. Screen Size 4. Aniamtion

--- app/src/main/java/com/example/fooddelivery/ui/customviews/bottomsheet/CustomSheet.kt
+++ app/src/main/java/com/example/fooddelivery/ui/customviews/bottomsheet/CustomSheet.kt
@@ -1,23 +1,33 @@
 package com.example.fooddelivery.ui.customviews.bottomsheet
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
 
 class BottomSheet {
 }
 
 @Composable
-fun TransformableSample() {
+fun CustomSheet(
+    INNER_VIEW : @Composable (Constraints) -> Unit
+) {
     // set up all transformation states
-    var scale by remember { mutableStateOf(1f) }
+    var scale by remember { mutableStateOf(0f) }
     var rotation by remember { mutableStateOf(0f) }
     var offset by remember { mutableStateOf(Offset.Zero) }
     val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
@@ -25,8 +35,35 @@ fun TransformableSample() {
         rotation += rotationChange
         offset += offsetChange
     }
-    Box(
-        Modifier
+
+    var backgroundClick by remember { mutableStateOf(300)}
+    BoxWithConstraints(
+        contentAlignment = Alignment.BottomCenter,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Red)
+            .height(height = if (backgroundClick == 0) 0.dp else 400.dp)
+            .clickable {
+                backgroundClick = 0
+            }
+    ) {
+
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(PaddingValues(start = 5.dp, end = 5.dp, top = 5.dp, bottom = 0.dp))
+                .fillMaxWidth()
+                .border(width = 1.dp, color = Color.Black, shape = RectangleShape)
+        ){
+            val boxScope = this@BoxWithConstraints
+            INNER_VIEW(boxScope.constraints)
+        }
+    }
+}
+
+/*
+Modifier
             // apply other transformations like rotation and zoom
             // on the pizza slice emoji
             .graphicsLayer(
@@ -41,9 +78,6 @@ fun TransformableSample() {
             .transformable(state = state)
             .background(Color.Blue)
             .fillMaxHeight()
-    )
-}
 
-/*
 https://ichi.pro/ko/jetpack-composeleul-sayonghayeo-bottomsheet-kontencheue-aenimeisyeon-eul-jeog-yonghaneun-bangbeob-68906696522716
  */

797a7d02443a55b72ae9c2f3ff933d2977877686
JuYeon Lee <cavss1118@gmail.com>
2021-10-30 23:00:02 +0900

    #0081, TopBarView // 1. give underline below topbar lazyrow

---
+++ app/src/main/java/com/example/fooddelivery/ui/customviews/isometric/IsometricView.kt
@@ -0,0 +1,17 @@
+package com.example.fooddelivery.ui.customviews.isometric
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun IsometricView(
+//    content : @Composable () -> Unit
+) {
+    Box(
+        modifier = Modifier
+    ) {
+
+    }
+}
--- app/src/main/java/com/example/fooddelivery/ui/customviews/topbar/TopBarView.kt
+++ app/src/main/java/com/example/fooddelivery/ui/customviews/topbar/TopBarView.kt
@@ -1,15 +1,22 @@
 package com.example.fooddelivery.ui.customviews.topbar
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.ui.theme.UbuntuRed
 import com.example.fooddelivery.util.strings.Strings
 
 @Composable
@@ -18,24 +25,42 @@ fun TopBarView(
     setHeight : Dp,
     setOnClick : (model : TopBarModel) -> Unit
 ){
-    // 메모리 관리가 들어간 lazyColumn
-    LazyRow(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start,
-        reverseLayout = false,
-        contentPadding = PaddingValues(horizontal = 0.dp),
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(setHeight)
-            .background(Color.White)
-    ){
-        items(setList){ model ->
-            TopBarItemView(
-                model = model,
-                setOnClick = { topBarModel ->
-                    setOnClick(topBarModel)
-                }
-            )
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        // 메모리 관리가 들어간 lazyColumn
+        LazyRow(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            reverseLayout = false,
+            contentPadding = PaddingValues(horizontal = 0.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(setHeight)
+                .background(Color.White)
+        ){
+            items(setList){ model ->
+                TopBarItemView(
+                    model = model,
+                    setOnClick = { topBarModel ->
+                        setOnClick(topBarModel)
+                    }
+                )
+            }
         }
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp),
+            onDraw = {
+                drawLine(
+                    color = Color.Black,
+                    start = Offset(0f, 0f),
+                    end = Offset(size.width ,0f),
+                    strokeWidth = 1f
+                )
+            }
+        )
     }
 }

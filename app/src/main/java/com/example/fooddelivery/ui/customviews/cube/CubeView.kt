e8c7a0f7170b576bdfa3e7e3046717153db8e1c8
JuYeon Lee <cavss1118@gmail.com>
2021-11-02 23:51:04 +0900

    #0083, CubeView // 1. 좌우 스크롤 애니메이션을 넣었지만 좀 더 세세하게 작업해야한다

--- app/src/main/java/com/example/fooddelivery/ui/customviews/cube/CubeView.kt
+++ app/src/main/java/com/example/fooddelivery/ui/customviews/cube/CubeView.kt
@@ -1,59 +1,118 @@
 package com.example.fooddelivery.ui.customviews.cube
 
+import android.util.Log
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CubeView(
     width : Dp,
     height : Dp
 ) {
+
+    var offset by remember { mutableStateOf(0f) }
+
     Box(
         modifier = Modifier
             .size(width, height)
             .background(Color.White)
+            .scrollable(
+                orientation = Orientation.Horizontal,
+                // Scrollable state: describes how to consume
+                // scrolling delta and update offset
+                state = rememberScrollableState { delta ->
+                    offset += delta
+                    delta
+                }
+            )
     ) {
         Box(
             modifier = Modifier
-                .size(width, height)
-                .background(Color.Blue)
+                .size(width - 100.dp, height - 100.dp)
                 .border(1.dp, Color.Black, RectangleShape)
+                .align(Alignment.Center)
+                .graphicsLayer(
+                    transformOrigin = TransformOrigin(0.0f, 0.5f),
+//                    scaleX = scale,
+//                    scaleY = 1f,
+                    rotationY = offset,
+                    translationX = 100f,
+//                    translationY = 1f
+                )
         ) {
-
+            Text(
+                text = "left",
+                modifier = Modifier
+                    .size(width - 105.dp, height - 105.dp)
+                    .border(1.dp, Color.Black, RectangleShape),
+                fontSize = 50.sp
+            )
         }
-
-        val customRotateYRightDoor = animateFloatAsState(
-            targetValue =  10f
-//            ,
-//            animationSpec = tween(
-//                durationMillis = 300
-//            )
-        )
-        Box(
-            modifier = Modifier
-                .size(width, height)
-                .background(Color.Red)
-                .border(1.dp, Color.Black, RectangleShape)
-                .graphicsLayer {
-                    transformOrigin = TransformOrigin(1.0f, 0.5f)
-                    rotationY = customRotateYRightDoor.value
-                }
-        ) {
-
-        }
-
     }
-
 }
+
+//var offsetX by remember { mutableStateOf(0f) }
+//Box(
+//modifier = Modifier
+//.pointerInput(Unit) {
+//    detectDragGestures { change, dragAmount ->
+//        change.consumeAllChanges()
+//        offsetX += dragAmount.x
+//        Log.e("mException", "dragAmount.x : ${dragAmount.x}")
+//    }
+//},
+//                .draggable(
+//                    orientation = Orientation.Horizontal,
+//                    // Scrollable state: describes how to consume
+//                    // scrolling delta and update offset
+//                    state = rememberDraggableState { delta ->
+//                        offsetX += delta
+//                        Log.e("mException", "delta : ${delta}")
+//                    }
+//                ),
+//contentAlignment = Alignment.Center
+//) {
+//    Box(
+//        modifier = Modifier
+//            .width(maxWidth)
+//            .height((maxHeight / 5) * 4)
+//            .clip(RectangleShape)
+//            .background(Color.Red)
+//    )
+//            Box(
+//                modifier = Modifier
+//                    .width(maxWidth)
+//                    .height((maxHeight/5) * 4)
+//                    .clip(RectangleShape)
+//                    .background(Color.Yellow)
+//            )
+//
+//            Box(
+//                modifier = Modifier
+//                    .width(maxWidth)
+//                    .height((maxHeight/5) * 4)
+//                    .clip(RectangleShape)
+//                    .background(Color.Green)
+//            )
+//}
